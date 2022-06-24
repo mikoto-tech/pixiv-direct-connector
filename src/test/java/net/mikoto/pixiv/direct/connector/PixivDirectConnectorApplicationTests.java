03d80aef2b69b80f7401c9641c45c7cd557381ab
mikoto <mikoto@ungine.cn>
2022-06-25 01:09:05 +0800

    [init] Init repo

---
+++ src/test/java/net/mikoto/pixiv/direct/connector/PixivDirectConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mikoto.pixiv.direct.connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixivDirectConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
